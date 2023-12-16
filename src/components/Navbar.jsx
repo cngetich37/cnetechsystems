b7b07e66cdb2723b6fbfe0c7d0c9055269f237ff
cngetich37 <cngetich37@gmail.com>
2023-12-16 18:28:50 +0300

    added the navbar and the home page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,113 @@
+import cnetechImage from "../assets/cnetech.png";
+const Navbar = () => {
+  return (
+    <>
+      <main className="sticky top-0 z-50 bg-gray-900">
+        <div className="navbar relative  text-white">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div tabIndex={0} className="btn btn-ghost lg:hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow text-white hover:text-yellow-500 bg-gray-900 rounded-box w-48"
+              >
+                <li>
+                  <a className="font-semibold" to="/home">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a className="font-semibold" to="/aboutus">
+                    About Us
+                  </a>
+                </li>
+                <li>
+                  <a className="font-semibold" to="/services">
+                    Our Services
+                  </a>
+                </li>
+                <li>
+                  <a className="font-semibold" to="/contact">
+                    Contact Us
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <img src={cnetechImage} className="h-8" />
+            <a
+              to="/"
+              className="btn btn-ghost normal-case text-xl font-mono hover:bg-yellow-500 hover:text-gray-900"
+            >
+              CNETECH SYSTEMS
+            </a>
+          </div>
+          <>
+            <div className="navbar-center hidden lg:flex ">
+              <ul className="menu menu-horizontal px-1 ">
+                <li>
+                  <a
+                    className="hover:bg-yellow-500 hover:text-gray-900 font-semibold"
+                    to="/home"
+                  >
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="hover:bg-yellow-500 hover:text-gray-900 font-semibold"
+                    to="/aboutus"
+                  >
+                    About Us
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="hover:bg-yellow-500 hover:text-gray-900 font-semibold"
+                    to="/services"
+                  >
+                    Our Services
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="hover:bg-yellow-500 hover:text-gray-900 font-semibold"
+                    to="/contactus"
+                  >
+                    Contact Us
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div className="navbar-end flex gap-2">
+              <button
+                to="/login"
+                className="rounded-md px-4 py-2 text-gray-900 bg-yellow-500 font-mono font-semibold hover:text-[#800000] hover:bg-gray-200"
+                type="submit"
+              >
+                Get Started
+              </button>
+            </div>
+          </>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,47 @@
+import cnetechImage from "../assets/cnetech.png";
+const HomePage = () => {
+  const styles = {
+    container: {
+      backgroundImage: `url(${cnetechImage})`,
+      backgroundSize: "contain", // You can use 'contain' or a specific size as well
+      height: "400px", // Adjust the height as needed
+      // You can add more styling properties as per your requirements
+    },
+  };
+  return (
+    <>
+      <main
+        className="hero min-h-screen object-scale-down bg-white"
+        style={styles.container}
+      >
+        <div className="hero-overlay bg-opacity-80">
+          <div className="hero-content text-center">
+            <div className="max-w-md font-serif">
+              <h1 className="text-5xl font-bold text-white font-mono mt-8">
+                CNETECH SYSTEMS
+              </h1>
+              <p className="py-8 text-white font-mono font-semibold">
+                CNETECH SYSTEMS: Revolutionizing the digital landscape with
+                cutting-edge ingenuity! We're not just developers; we're
+                architects of seamless experiences. Specializing in MERN stack
+                technology, we sculpt web and mobile apps that transcend
+                expectations. Elevate your digital presence with our
+                innovation-driven solutions, where user-centric design meets
+                robust functionality. Your vision, our expertise—unleash the
+                power of possibilities with CNETECH SYSTEMS!
+              </p>
+              <a
+                to="/"
+                className="rounded-md text-md font-mono font-bold bg-yellow-500 text-gray-900 hover:text-[#800000] hover:bg-gray-200 py-4 px-4"
+              >
+                Get Started
+              </a>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default HomePage;
