0249f93ff40e51bb83ec3890dd8a95d5f99e5efd
cngetich37 <cngetich37@gmail.com>
2023-12-17 15:51:39 +0300

    added the navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
-
+import AboutUs from "./pages/AboutUs";
+import OurServices from "./pages/OurServices";
+import ContactUs from "./pages/ContactUs";
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
       <Navbar />
-      <HomePage />
+      <Routes>
+        <Route path="/" exact element={<HomePage />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/services" element={<OurServices />} />
+        <Route path="/contactus" element={<ContactUs />} />
+      </Routes>
       <Footer />
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import cnetechImage from "../assets/cnetech.png";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
-      <main className="sticky top-0 z-50 bg-gray-900">
+      <nav className="sticky top-0 z-50 bg-gray-900">
         <div className="navbar relative  text-white">
           <div className="navbar-start">
             <div className="dropdown">
@@ -27,82 +28,82 @@ const Navbar = () => {
                 className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow text-white hover:text-yellow-500 bg-gray-900 rounded-box w-48"
               >
                 <li>
-                  <a className="font-semibold" to="/home">
+                  <Link className="font-semibold" to="/home">
                     Home
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a className="font-semibold" to="/aboutus">
+                  <Link className="font-semibold" to="/aboutus">
                     About Us
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a className="font-semibold" to="/services">
+                  <Link className="font-semibold" to="/services">
                     Our Services
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a className="font-semibold" to="/contact">
+                  <Link className="font-semibold" to="/contact">
                     Contact Us
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </div>
             <img src={cnetechImage} className="h-8" />
-            <a
+            <Link
               to="/"
               className="btn btn-ghost normal-case text-xl font-mono hover:bg-yellow-500 hover:text-gray-900"
             >
               CNETECH SYSTEMS
-            </a>
+            </Link>
           </div>
           <>
             <div className="navbar-center hidden lg:flex ">
               <ul className="menu menu-horizontal px-1 ">
                 <li>
-                  <a
+                  <Link
                     className="hover:bg-yellow-500 hover:text-gray-900 font-semibold"
-                    to="/home"
+                    to="/"
                   >
                     Home
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a
+                  <Link
                     className="hover:bg-yellow-500 hover:text-gray-900 font-semibold"
                     to="/aboutus"
                   >
                     About Us
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a
+                  <Link
                     className="hover:bg-yellow-500 hover:text-gray-900 font-semibold"
                     to="/services"
                   >
                     Our Services
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a
+                  <Link
                     className="hover:bg-yellow-500 hover:text-gray-900 font-semibold"
                     to="/contactus"
                   >
                     Contact Us
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </div>
 
             <div className="navbar-end flex gap-2">
-              <a
+              <Link
                 to="#"
                 className="rounded-md px-4 py-2 text-gray-900 bg-gray-900 font-mono font-semibold hover:text-gray-900 hover:bg-gray-900"
-              ></a>
+              ></Link>
             </div>
           </>
         </div>
-      </main>
+      </nav>
     </>
   );
 };
