26cc74afb655c56849c04f2426c6985c534f3e30
cngetich37 <cngetich37@gmail.com>
2023-12-18 13:01:02 +0300

    updated the app and Missing Page components

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import AboutUs from "./pages/AboutUs";
 import OurServices from "./pages/OurServices";
 import ContactUs from "./pages/ContactUs";
 import { Route, Routes } from "react-router-dom";
+import MissingPage from "./components/MissingPage";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/aboutus" element={<AboutUs />} />
         <Route path="/services" element={<OurServices />} />
         <Route path="/contactus" element={<ContactUs />} />
+        <Route path="*" element={<MissingPage />} />
       </Routes>
       <Footer />
     </>
---
+++ src/components/MissingPage.jsx
@@ -0,0 +1,15 @@
+const MissingPage = () => {
+    return (
+      <>
+        <div className="hero h-screen bg-white flex items-start justify-center">
+          <div className="hero-content mt-20">
+            <div className="text-gray-900 text-4xl font-bold">
+              Something went wrong!
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
+  
+  export default MissingPage;
